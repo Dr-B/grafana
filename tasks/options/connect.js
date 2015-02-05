407a3d1d3287f6e46965e6f961741c974a6e1c64
Aaron Bostwick <aboswick@lbl.gov>
2015-02-05 09:26:29 -0800

    Add Access-Control-Allow-Origin * to test server

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -1,20 +1,32 @@
 module.exports = function(config) {
-  return {
-    dev: {
-      options: {
-        port: 5601,
-        hostname: '*',
-        base: config.srcDir,
-        keepalive: true
-      }
-    },
-    dist: {
-      options: {
-        port: 5605,
-        hostname: '*',
-        base: config.destDir,
-        keepalive: true
-      }
-    },
-  }
+    return {
+        dev: {
+            options: {
+                port: 5601,
+                hostname: '*',
+                base: config.srcDir,
+                keepalive: true,
+                middleware: function(connect, options, middlewares) {
+                    // inject a custom middleware 
+                    middlewares.unshift(function(req, res, next) {
+                        res.setHeader('Access-Control-Allow-Origin', '*');
+                        res.setHeader('Access-Control-Allow-Methods', '*');
+                        //a console.log('foo') here is helpful to see if it runs
+                        return next();
+                    });
+
+                    return middlewares;
+                }
+
+            }
+        },
+        dist: {
+            options: {
+                port: 5605,
+                hostname: '*',
+                base: config.destDir,
+                keepalive: true
+            }
+        },
+    }
 };
